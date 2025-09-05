cdabee7c87ee5ffe9cc462420071e4099e3640b3
priyansh18 <singhalpriyansh58@gmail.com>
2025-09-05 15:40:32 +0530

    feat: credits API, transaction model, and user route updates 💳

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { getUser, loginUser, registerUser } from "../controllers/userController.js";
+import {
+  getPublishedImages,
+  getUser,
+  loginUser,
+  registerUser,
+} from "../controllers/userController.js";
 import { protect } from "../middlewares/auth.js";
 
 const userRouter = express.Router();
@@ -7,5 +12,6 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/data", protect, getUser);
+userRouter.get("/publishedImages", getPublishedImages);
 
 export default userRouter;
